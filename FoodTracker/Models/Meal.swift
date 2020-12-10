3db23bb67f2dded076b03ba831fa865a02eb767b
George Nyakundi <dev.things.ke@gmail.com>
2020-12-10 09:40:31 +0300

    feat(persist): adding persistance code

--- FoodTracker/Models/Meal.swift
+++ FoodTracker/Models/Meal.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSCoding {
     
     //MARK: Properties
     
@@ -15,8 +16,19 @@ class Meal {
     var photo: UIImage?
     var rating: Int
     
+    // MARK: Archiving Paths
+    static let DocumentDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentDirectory.appendingPathComponent("meals")
     
-
+    
+    // MARK: Types
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
+    
     
     //MARK: Initialization
     init?(name: String, photo:UIImage?, rating: Int) {
@@ -30,5 +42,27 @@ class Meal {
         self.name = name
         self.photo = photo
         self.rating = rating
+    }
+    
+    // MARK: NSCoding
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: PropertyKey.name)
+        coder.encode(photo,forKey: PropertyKey.photo)
+        coder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder: NSCoder) {
+        // The name is required. If we cannot decode a name string, the initializer should fail
+        guard let name = coder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name from the stored meal object.", log:OSLog.default, type: .default)
+            return nil
+        }
+        
+        // Because photo is an optional property of Meal, just use conditional cast
+        let photo = coder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        let rating = coder.decodeInteger(forKey: PropertyKey.rating)
+        
+        self.init(name:name, photo:photo, rating:rating)
     }
 }
